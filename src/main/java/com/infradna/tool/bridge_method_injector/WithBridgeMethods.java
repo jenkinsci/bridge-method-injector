9da4c52e138a99f55493232519f05c5b34276660
Kohsuke Kawaguchi <kk@kohsuke.org>
2010-08-06 13:04:49 -0700

    doc improvement.

--- src/main/java/com/infradna/tool/bridge_method_injector/WithBridgeMethods.java
+++ src/main/java/com/infradna/tool/bridge_method_injector/WithBridgeMethods.java
@@ -10,8 +10,26 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.CLASS;
 
 /**
- * Request that synthetic methods of the same name and same arguments be generated
- * with each specified type as the return type.
+ * Request that bridge methods of the same name and same arguments be generated
+ * with each specified type as the return type. This helps you maintain binary compatibility
+ * as you evolve your classes.
+ *
+ * <p>
+ * For example, if you have the following code:
+ *
+ * <pre>
+ * &#64;WithBridgeMethods(Foo.class)
+ * public FooSubType getFoo() { ... }
+ * </pre>
+ *
+ * <p>
+ * The Maven mojo will insert the following bridge method:
+ *
+ * <pre>
+ * public Foo getFoo() {
+ *     return getFoo(); // invokevirtual to getFoo() that returns FooSubType
+ * }
+ * </pre>
  *
  * @author Kohsuke Kawaguchi
  */
