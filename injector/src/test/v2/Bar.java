0ba19158f85c3ab6208db6167618b2a693910e52
Basil Crow <me@basilcrow.com>
2023-08-03 14:45:43 -0700

    Add tests for interfaces

--- injector/src/test/v2/Bar.java
+++ injector/src/test/v2/Bar.java
@@ -1,4 +1,25 @@
+import com.infradna.tool.bridge_method_injector.WithBridgeMethods;
+import java.io.UncheckedIOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Bar implements IBar {
+    @WithBridgeMethods(value = String.class, castRequired = true)
     public Object widen()  { return "bar"; }
+
+    @WithBridgeMethods(Object.class)
     public String narrow() { return "bar"; }
+
+    @WithBridgeMethods(value = String.class, adapterMethod = "convert")
+    public URL adapter() {
+        try {
+            return new URL("http://example.com/");
+        } catch (MalformedURLException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private Object convert(URL url, Class<?> type) {
+        return url.toString();
+    }
 }
